b8793e12691a2f4551eb9f06e2fe7fed3bcad42f
dyooreen <dyooreen@icloud.com>
2024-01-18 01:04:26 +0400

    change styles

--- src/App.tsx
+++ src/App.tsx
@@ -34,25 +34,35 @@ function App() {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
   return (
-    <Container maxW="2xl" centerContent alignItems={"center"}>
-      <InputGroup size="lg" marginY={"10"}>
-        <Input
-          onChange={(e) => setNewTodo(e.target.value)}
-          value={newTodo}
-          placeholder="what need to do"
-        />
-        <InputRightElement width="4.5rem">
-          <Button onClick={addItem} h="1.75rem" size="sm">
-            <AddIcon />
-          </Button>
-        </InputRightElement>
-      </InputGroup>
+    <Container maxW="2xl" alignItems={"center"}>
+      <form>
+        <InputGroup size="lg" marginY={"10"} alignItems={"center"}>
+          <Input
+            onChange={(e) => setNewTodo(e.target.value)}
+            height="4.5rem"
+            value={newTodo}
+            placeholder="what need to do"
+          />
+          <InputRightElement height="4.5rem" width="4.5rem">
+            <Button
+              onClick={addItem}
+              type="submit"
+              height="4.3rem"
+              width="4.3rem"
+            >
+              <AddIcon />
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </form>
       {todos.map((todo) => (
         <Card key={todo.id} variant={"outline"} width={"100%"} marginY={"4"}>
           <CardBody>
             <Flex justifyContent={"space-between"} alignItems={"center"}>
               <Text>{todo.text}</Text>
-              <DeleteIcon onClick={() => deleteItem(todo.id)} boxSize={4} />
+              <Button>
+                <DeleteIcon onClick={() => deleteItem(todo.id)} boxSize={4} />
+              </Button>
             </Flex>
           </CardBody>
         </Card>
